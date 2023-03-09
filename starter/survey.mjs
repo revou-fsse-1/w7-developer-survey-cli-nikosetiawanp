cb2df67f095f11a9219f4f023dc679a89a0e3ec5
nikosetiawanp <nikosetiawanp@gmail.com>
2023-03-09 23:06:26 +0700

    finalized expectedsalary

--- starter/survey.mjs
+++ starter/survey.mjs
@@ -24,11 +24,11 @@ const questions = [
       return `Hello ${answer.firstName} what is your email address?`
     },
     validate: (answer) => {
-      if (!answer) {
-      return "Please enter your email address";
-      }
-      return true;
-      }
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(answer)) {
+        return "Please enter correct email address"
+      } return true;
+    }
   },
 
   {
@@ -36,7 +36,7 @@ const questions = [
     name: "experience",
     message: "Are you experienced developer?",
     choices: ["Experienced", "Inexperienced"],
-    when: ()
+
   },
 
   {
@@ -44,21 +44,49 @@ const questions = [
     name: "yearsOfExperience",
     message: "Nice! How many years of experience do you have?",
     choices: ["0-1", "1-3", "3-5", "5-10", "10+"],
-  }
+  },
+
+  {
+    type: "checkbox",
+    name: "javascriptLibrary",
+    message: "What Javascript library do you know?",
+    choices: ["React", "Vue", "Angular", "NodeJS", "JQuery", "D3.JS"],
+  },
+
+  {
+    type: "input",
+    name: "expectedSalary",
+    message: "What is your expected salary? (in Rupiah)",
+    validate : (answer) => {
+      const numberRegex = /^\d+$/;;
+
+      if (!numberRegex.test(answer) || !answer || answer <= 0) {
+        return "Input Invalid, your expected salary should be greater than 0"
+      } else if (answer > 10000000) {
+        return "Sorry, that is out of our budget!"
+      } else {
+        return true
+      }
+    }
+  },
 
 ];
 
 // run your command
 inquirer
   .prompt(questions)
+
   .then((answers) => {
     console.log(JSON.stringify(answers, null, 2));
   })
+
   .catch((error) => {
     if (error.isTtyError) {
       console.log("Your console environment is not supported!");
     } else {
       console.log(error);
     }
-  });
+  })
+  
+  ;
   
